9453cb015ed2dedc183a5a612bc054a5bccd6b25
Andrey Serikov <AS.Korag@protonmail.com>
2021-10-25 16:30:01 +0300

    fix: range task 120/150

--- js-basics/src/range.ts
+++ js-basics/src/range.ts
@@ -1,62 +1,48 @@
-module.exports = function range(from: number, to?: number, step?: number): number[] {
+module.exports = function range(from: number, ...rest: number[]): number[] {
+  const res: number[] = []
 
-  // validateArgs(from, to, step)
-
-  const result: number[] = []
-
-  if ((from < to && step < 0) || (from > to && step > 0)) {
-    return []
-  }
-
-  if (to === undefined) {
+  if (isNormDig(from) && rest.length === 0) {
     for (let i = 0; i < from; i++) {
-      result.push(i)
+      res.push(i)
     }
+    return res
   }
 
-  if (step === undefined) {
+  const to = rest[0]
+  if (isNormDig(from) && isNormDig(to) && rest.length === 1) {
     if (from < to) {
       for (let i = from; i < to; i++) {
-        result.push(i!)
+        res.push(i)
       }
     } else {
       for (let i = from; i > to; i--) {
-        result.push(i!)
+        res.push(i)
       }
     }
-  } else {
-
-
-    step = Math.abs(step)
-    if (from < to) {
-      for (let i = from; i < to; i = i + step) {
-        result.push(i!)
+    return res
+  }
+  let step = rest[1]
+  if (isNormDig(from) && isNormDig(to) && isNormDig(step) && rest.length === 2) {
+    if ((from < to && step < 0) || (from > to && step > 0)) {
+      return []
+    }
+    if ((from < to && step > 0) || (from > to && step < 0)) {
+      if (from < to) {
+        for (let i = from; i < to; i = i + step) {
+          res.push(i)
+        }
+      } else {
+        for (let i = from; i > to; i = i + step) {
+          res.push(i)
+        }
       }
-    } else {
-      for (let i = from; i > to; i = i - step) {
-        result.push(i!)
-      }
+      return res
     }
   }
 
-
-  return result
+  return []
 }
 
-// function validateArgs(from: number, to?: number, step?: number) {
-//   /*if (Number.isNaN(from) || Number.isNaN(to) || Number.isNaN(step)) {
-//     throw new Error('Arguments must be number')
-//   }*/
-//   if (from === undefined) {
-//     throw new Error('Must be 1 or more arguments')
-//   }
-//   if (from < 0 && to === undefined) {
-//     throw new Error('Length must be more than 0')
-//   }
-//   if (step === 0) {
-//     throw new Error(`Step can't be equal 0`)
-//   }
-//   if (Number.isNaN(+from)) {
-//     throw new Error('Length must be number')
-//   }
-// }
+function isNormDig(arg?: number) {
+  return (arg !== undefined && arg !== null && !Number.isNaN(arg) && Number.isInteger(arg))
+}
--- js-basics/tests/range.test.js
+++ js-basics/tests/range.test.js
@@ -1,58 +1,91 @@
 const range = require('../dist/range')
 
 test('Run without arguments', () => {
-    expect(() => {
-        range()
-    }).toThrow(new Error('Must be 1 or more arguments'))
+    expect(range()).toEqual([])
 })
 
-/*test('Run with incorrect arguments', () => {
-    expect(() => {
-        range('7u')
-    }).toThrow(new Error('Arguments must be number'))
-})*/
-
-test('Run with 1 positive arguments', () => {
+test('Run with 1 argument only', () => {
     expect(range(0)).toEqual([])
-    expect(range(2)).toEqual([0, 1])
     expect(range(5)).toEqual([0, 1, 2, 3, 4])
+    expect(range(9)).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8])
+    expect(range(-5)).toEqual([])
+    expect(range(-89)).toEqual([])
 })
 
-test('Run with 1 negative arguments', () => {
-    expect(() => {
-        range(-2)
-    }).toThrow(new Error('Length must be more than 0'))
-    expect(() => {
-        range(-10)
-    }).toThrow(new Error('Length must be more than 0'))
+test('Run with 1 correct argument', () => {
+    expect(range(-5, undefined)).toEqual([])
+    expect(range(-5, null)).toEqual([])
+    expect(range(-5, NaN)).toEqual([])
+    expect(range(-5, Infinity)).toEqual([])
 })
 
-test('Run with 2 arguments, forward direction', () => {
-    expect(range(10, 16)).toEqual([10, 11, 12, 13, 14, 15])
-    expect(range(0, 5)).toEqual([0, 1, 2, 3, 4])
-    expect(range(-3, 2)).toEqual([-3, -2, -1, 0, 1])
-    expect(range(-10, -7)).toEqual([-10, -9, -8])
+test('Run with 2 arguments only', () => {
+    expect(range(1, 9)).toEqual([1, 2, 3, 4, 5, 6, 7, 8])
+    expect(range(10, 15)).toEqual([10, 11, 12, 13, 14])
+    expect(range(-5, 5)).toEqual([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4])
+    expect(range(-10, -5)).toEqual([-10, -9, -8, -7, -6])
+    expect(range(-6, 1)).toEqual([-6, -5, -4, -3, -2, -1, 0])
+    expect(range(9, 1)).toEqual([9, 8, 7, 6, 5, 4, 3, 2])
+    expect(range(15, 10)).toEqual([15, 14, 13, 12, 11])
+    expect(range(5, -5)).toEqual([5, 4, 3, 2, 1, 0, -1, -2, -3, -4])
+    expect(range(-5, -10)).toEqual([-5, -6, -7, -8, -9])
+    expect(range(1, -6)).toEqual([1, 0, -1, -2, -3, -4, -5])
 })
 
-test('Run with 2 arguments, reverse direction', () => {
-    expect(range(16, 10)).toEqual([16, 15, 14, 13, 12, 11])
-    expect(range(5, 0)).toEqual([5, 4, 3, 2, 1])
-    expect(range(2, -3)).toEqual([2, 1, 0, -1, -2])
-    expect(range(-7, -10)).toEqual([-7, -8, -9])
+test('Run with 2 correct arguments', () => {
+    expect(range(1, 9, null)).toEqual([])
+    expect(range(10, 15, undefined)).toEqual([])
+    expect(range(-5, 5, NaN)).toEqual([])
+    expect(range(-10, -5, Infinity)).toEqual([])
+    expect(range(1, null, 1)).toEqual([])
+    expect(range(10, undefined, 15)).toEqual([])
+    expect(range(-5, NaN, 5)).toEqual([])
+    expect(range(-10, Infinity, -5)).toEqual([])
 })
 
-test('Run with 3 arguments, forward direction', () => {
-    expect(range(10, 16, 1)).toEqual([10, 11, 12, 13, 14, 15])
-    expect(range(0, 5, 2)).toEqual([0, 2, 4])
-    expect(range(-3, 2, 1)).toEqual([-3, -2, -1, 0, 1])
-    expect(range(-20, -7, 3)).toEqual([-20, -17, -14, -11, -8])
+
+// test('Run with 1 bigInt argument', () => {
+//     expect(range(0n)).toEqual([])
+//     expect(range(5n)).toEqual([0n, 1n, 2n, 3n, 4n])
+//     expect(range(9n)).toEqual([0n, 1n, 2n, 3n, 4n, 5n, 6n, 7n, 8n])
+//     expect(range(-5n)).toEqual([])
+//     expect(range(-89n)).toEqual([])
+// })
+
+
+test('Run with 3 arguments, correct step', () => {
+    expect(range(5, 10, 1)).toEqual([5, 6, 7, 8, 9])
+    expect(range(5, 10, 2)).toEqual([5, 7, 9])
+    expect(range(-5, 5, 2)).toEqual([-5, -3, -1, 1, 3])
+    expect(range(-10, -5, 3)).toEqual([-10, -7])
+    expect(range(10, 5, -1)).toEqual([10, 9, 8, 7, 6])
+    expect(range(10, 5, -2)).toEqual([10, 8, 6])
+    expect(range(5, -5, -2)).toEqual([5, 3, 1, -1, -3])
+    expect(range(-5, -10, -3)).toEqual([-5, -8])
 })
 
-test('Run with 3 arguments, reverse direction', () => {
-    expect(range(16, 10, -1)).toEqual([16, 15, 14, 13, 12, 11])
-    expect(range(5, 0, -2)).toEqual([5, 3, 1])
-    expect(range(2, -3, -1)).toEqual([2, 1, 0, -1, -2])
-    expect(range(-7, -20, -3)).toEqual([-7, -10, -13, -16, -19])
+test('Run with 3 arguments, incorrect step', () => {
+    expect(range(5, 10, -1)).toEqual([])
+    expect(range(5, 10, -2)).toEqual([])
+    expect(range(-5, 5, -2)).toEqual([])
+    expect(range(-10, -5, -3)).toEqual([])
+    expect(range(10, 5, 1)).toEqual([])
+    expect(range(10, 5, 2)).toEqual([])
+    expect(range(5, -5, 2)).toEqual([])
+    expect(range(-5, -10, 3)).toEqual([])
+    expect(range(-5, -10, null)).toEqual([])
+    expect(range(-5, -10, NaN)).toEqual([])
+    expect(range(-5, -10, undefined)).toEqual([])
+    expect(range(-5, -10, Infinity)).toEqual([])
+    expect(range(-5, 2, Infinity)).toEqual([])
+
 })
+
+test('Run with 3 arguments, step is 0', () => {
+    expect(range(5, 10, 0)).toEqual([])
+    expect(range(5, 10, undefined)).toEqual([])
+
+})
+
 
 
