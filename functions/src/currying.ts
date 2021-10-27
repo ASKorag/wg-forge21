5c5d9c455b7680f8b38e52ca5e7bc8a6b9020274
Andrey Serikov <AS.Korag@protonmail.com>
2021-10-27 16:18:23 +0300

    feat: currying task

--- functions/src/currying.ts
+++ functions/src/currying.ts
@@ -1,3 +1,16 @@
-module.exports = function curry() {
-  
+module.exports = function curry(func: TFunc): TFunc {
+  if (typeof func !== 'function') {
+    throw new TypeError('Argument must be a function')
+  }
+
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func(...args)
+    }
+    return function (...rest) {
+      return curried.apply(this, [...args, ...rest])
+    }
+  }
 }
+
+type TFunc = (...args: any[]) => any
--- functions/test/currying.test.js
+++ functions/test/currying.test.js
@@ -1,1 +1,25 @@
 const curry = require('../dist/currying.js')
+
+const point = (x, y, z) => [x, y, z]
+const curried = curry(point)
+
+test('Run with incorrect argument', () => {
+  expect(() => {
+    curry(1)
+  }).toThrow(new TypeError('Argument must be a function'))
+
+  expect(() => {
+    curry(true)
+  }).toThrow(new TypeError('Argument must be a function'))
+
+  expect(() => {
+    curry({})
+  }).toThrow(new TypeError('Argument must be a function'))
+})
+
+test('Run ...', () => {
+  expect(curried(1, 2, 3, 4)).toEqual([1, 2, 3])
+  expect(curried(1)(2)(3)).toEqual([1, 2, 3])
+  expect(curried(1, 2)(3)).toEqual([1, 2, 3])
+  expect(curried(1, 2, 3)).toEqual([1, 2, 3])
+})
