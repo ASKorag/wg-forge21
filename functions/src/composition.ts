6a91ae5143b893e4f2bbdcccbe041e895fbacd7b
Andrey Serikov <AS.Korag@protonmail.com>
2021-10-27 15:28:05 +0300

    feat: composition task

--- functions/src/composition.ts
+++ functions/src/composition.ts
@@ -1,3 +1,4 @@
-module.exports = function pipe() {
+module.exports = (funcArr: Array<TFunc>): TBasic => arg => funcArr.reduce((res, func) => func(res), arg)
 
-}
+type TBasic = (arg: any) => any
+type TFunc = (...args: any[]) => (arg: any) => any
--- functions/test/composition.test.js
+++ functions/test/composition.test.js
@@ -1,1 +1,18 @@
 const pipe = require('../dist/composition.js')
+
+const add = y => x => x + y
+const sub = y => x => x - y
+const mul = y => x => x * y
+const div = y => x => x / y
+
+//  (x + 100) * 3 / 4 - 1
+const firstOPD = 100
+const secOPD = 3
+const thirdOPD = 4
+const fourthOPD = 1
+const result = sub(fourthOPD)(div(thirdOPD)(mul(secOPD)(add(firstOPD)(0))))
+
+test('Run example', () => {
+  const solve = pipe([add(firstOPD), mul(secOPD), div(thirdOPD), sub(fourthOPD)])
+  expect(solve(0)).toBe(result)
+})
