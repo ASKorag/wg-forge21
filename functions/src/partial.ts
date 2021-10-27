7bf3bd56e618d6d013e063033646d8d6f8be6380
Andrey Serikov <AS.Korag@protonmail.com>
2021-10-27 13:47:20 +0300

    feat: partial task

--- functions/src/partial.ts
+++ functions/src/partial.ts
@@ -1,3 +1,6 @@
-module.exports = function partial() {
-
+module.exports = function partial(func: (...rest: any[]) => any, ...args: any[]) {
+  if (typeof func !== 'function') {
+    throw new TypeError('First argument must be a function')
+  }
+  return (...rest: any[]) => func(...args, ...rest)
 }
--- functions/test/partial.test.js
+++ functions/test/partial.test.js
@@ -1,1 +1,51 @@
 const partial = require('../dist/partial.js')
+
+const vol4D = (a, b, c, d) => a * b * c * d
+
+const firstArg = 10
+const secArg = 2
+const thirdArg = 3
+const fourthArg = 1.55
+
+test('Run with incorrect first argument', () => {
+  expect(() => {
+    partial(42, 8, 8)
+  }).toThrowError(new TypeError('First argument must be a function'))
+  expect(() => {
+    partial('78', 8, 8)
+  }).toThrow(new TypeError('First argument must be a function'))
+  expect(() => {
+    partial({}, 8, 8)
+  }).toThrow(new TypeError('First argument must be a function'))
+  expect(() => {
+    partial(true, 8, 8)
+  }).toThrow(new TypeError('First argument must be a function'))
+})
+
+test('Run with 1 argument', () => {
+  const vol4D_1 = partial(vol4D, firstArg)
+  const vol4D_2 = partial(vol4D_1, secArg)
+  const vol4D_3 = partial(vol4D_2, thirdArg)
+  expect(vol4D_3(1.55)).toBe(vol4D(firstArg, secArg, thirdArg, fourthArg))
+})
+
+test('Run with 2 arguments', () => {
+  const vol4D_1 = partial(vol4D, firstArg, secArg)
+  expect(vol4D_1(thirdArg, fourthArg)).toBe(vol4D(firstArg, secArg, thirdArg, fourthArg))
+})
+
+test('Run with random amount of arguments', () => {
+  const vol4D_1 = partial(vol4D, firstArg)
+  expect(vol4D_1(secArg, thirdArg, fourthArg)).toBe(vol4D(firstArg, secArg, thirdArg, fourthArg))
+
+  const vol4D_2 = partial(vol4D, firstArg, secArg, thirdArg)
+  expect(vol4D_2(fourthArg)).toBe(vol4D(firstArg, secArg, thirdArg, fourthArg))
+
+  const vol4D_3 = partial(vol4D, firstArg)
+  const vol4D_4 = partial(vol4D_3, secArg)
+  expect(vol4D_4(thirdArg, fourthArg)).toBe(vol4D(firstArg, secArg, thirdArg, fourthArg))
+
+  const vol4D_5 = partial(vol4D, firstArg, secArg)
+  const vol4D_6 = partial(vol4D_5, thirdArg)
+  expect(vol4D_6(fourthArg)).toBe(vol4D(firstArg, secArg, thirdArg, fourthArg))
+})
