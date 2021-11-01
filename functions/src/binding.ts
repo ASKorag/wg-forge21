120d7a623e4279133bef4e51a912c80103f466fc
Andrey Serikov <AS.Korag@protonmail.com>
2021-11-01 16:30:30 +0300

    add: binding task with spread

--- functions/src/binding.ts
+++ functions/src/binding.ts
@@ -1,13 +1,43 @@
 module.exports = {
-  bind() {
-
+  bind(func: Func, context: any) {
+    const args: any[] = []
+    for (var i = 2; i < arguments.length; i++) {
+      args.push(arguments[i])
+    }
+    var uuid = Date.now()
+    context[uuid] = func
+    return function () {
+      const rest = []
+      for (var i = 0; i < arguments.length; i++) {
+        rest.push(arguments[i])
+      }
+      var result = context[uuid](...args, ...rest)
+      delete context[uuid]
+      return result
+    }
   },
 
-  call() {
-
+  call(func: Func, context: any) {
+    const args = []
+    for (var i = 2; i < arguments.length; i++) {
+      args.push(arguments[i])
+    }
+    var uuid = Date.now()
+    context[uuid] = func
+    var result = context[uuid](...args)
+    delete context[uuid]
+    return result
   },
 
-  apply() {
-
+  apply(func: Func, context: any) {
+    const args = arguments[2]
+    var uuid = Date.now()
+    context[uuid] = func
+    var result = context[uuid](...args)
+    delete context[uuid]
+    return result
   }
 }
+
+
+type Func = (...args: any[]) => any
--- functions/test/binding.test.js
+++ functions/test/binding.test.js
@@ -1,1 +1,26 @@
 const funcUtil = require('../dist/binding.js')
+
+const user = {
+  name: 'Andrey'
+}
+
+const age = 26
+const sex = 'male'
+
+function showInfo(age, sex) {
+  return `Name: ${this.name}, age: ${age}, sex: ${sex}`
+}
+
+test('Check apply method', () => {
+  expect(funcUtil.apply(showInfo, user, [age, sex])).toBe(showInfo.apply(user, [age, sex]))
+})
+
+test('Check call method', () => {
+  expect(funcUtil.call(showInfo, user, age, sex)).toBe(showInfo.call(user, age, sex))
+})
+
+test('Check bind method', () => {
+  expect(funcUtil.bind(showInfo, user, age, sex)()).toBe(showInfo.bind(user, age, sex)())
+  expect(funcUtil.bind(showInfo, user, age)(sex)).toBe(showInfo.bind(user, age)(sex))
+  expect(funcUtil.bind(showInfo, user)(age, sex)).toBe(showInfo.bind(user)(age, sex))
+})
