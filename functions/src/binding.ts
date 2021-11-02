6f31d092d28bacf4d51350550e8aecd48137956f
Andrey Serikov <AS.Korag@protonmail.com>
2021-11-02 16:17:39 +0300

    add: call method in binding task

--- functions/src/binding.ts
+++ functions/src/binding.ts
@@ -1,42 +1,47 @@
 module.exports = {
-  bind(func: Func, context: any) {
-    const args: any[] = []
-    for (var i = 2; i < arguments.length; i++) {
-      args.push(arguments[i])
-    }
-    var uuid = Date.now()
-    context[uuid] = func
-    return function () {
-      const rest = []
-      for (var i = 0; i < arguments.length; i++) {
-        rest.push(arguments[i])
-      }
-      var result = context[uuid](...args, ...rest)
-      delete context[uuid]
-      return result
-    }
-  },
+  // bind(func: Func, context: any) {
+  //   const args: any[] = []
+  //   for (var i = 2; i < arguments.length; i++) {
+  //     args.push(arguments[i])
+  //   }
+  //   var uuid = Date.now()
+  //   context[uuid] = func
+  //   return function () {
+  //     const rest = []
+  //     for (var i = 0; i < arguments.length; i++) {
+  //       rest.push(arguments[i])
+  //     }
+  //     var result = context[uuid](...args, ...rest)
+  //     delete context[uuid]
+  //     return result
+  //   }
+  // },
 
   call(func: Func, context: any) {
     const args = []
     for (var i = 2; i < arguments.length; i++) {
       args.push(arguments[i])
     }
-    var uuid = Date.now()
-    context[uuid] = func
-    var result = context[uuid](...args)
-    delete context[uuid]
-    return result
-  },
 
-  apply(func: Func, context: any) {
-    const args = arguments[2]
+    var _args = []
+    for (var i = 0; i < args.length; i++) {
+      _args.push('args[' + i + ']')
+    }
     var uuid = Date.now()
     context[uuid] = func
-    var result = context[uuid](...args)
+    var result = eval(`context[uuid](` + _args.join(',') + `)`)
     delete context[uuid]
     return result
   }
+
+  // apply(func: Func, context: any) {
+  //   const args = arguments[2]
+  //   var uuid = Date.now()
+  //   context[uuid] = func
+  //   var result = context[uuid](...args)
+  //   delete context[uuid]
+  //   return result
+  // }
 }
 
 
--- functions/test/binding.test.js
+++ functions/test/binding.test.js
@@ -4,6 +4,12 @@ const user = {
   name: 'Andrey'
 }
 
+const person = {
+  getName() {
+    return this.name
+  }
+}
+
 const age = 26
 const sex = 'male'
 
@@ -11,16 +17,17 @@ function showInfo(age, sex) {
   return `Name: ${this.name}, age: ${age}, sex: ${sex}`
 }
 
-test('Check apply method', () => {
-  expect(funcUtil.apply(showInfo, user, [age, sex])).toBe(showInfo.apply(user, [age, sex]))
-})
+// test('Check apply method', () => {
+//   expect(funcUtil.apply(showInfo, user, [age, sex])).toBe(showInfo.apply(user, [age, sex]))
+// })
 
 test('Check call method', () => {
   expect(funcUtil.call(showInfo, user, age, sex)).toBe(showInfo.call(user, age, sex))
+  expect(funcUtil.call(person.getName, user)).toBe(person.getName.call(user))
 })
 
-test('Check bind method', () => {
-  expect(funcUtil.bind(showInfo, user, age, sex)()).toBe(showInfo.bind(user, age, sex)())
-  expect(funcUtil.bind(showInfo, user, age)(sex)).toBe(showInfo.bind(user, age)(sex))
-  expect(funcUtil.bind(showInfo, user)(age, sex)).toBe(showInfo.bind(user)(age, sex))
-})
+// test('Check bind method', () => {
+//   expect(funcUtil.bind(showInfo, user, age, sex)()).toBe(showInfo.bind(user, age, sex)())
+//   expect(funcUtil.bind(showInfo, user, age)(sex)).toBe(showInfo.bind(user, age)(sex))
+//   expect(funcUtil.bind(showInfo, user)(age, sex)).toBe(showInfo.bind(user)(age, sex))
+// })
